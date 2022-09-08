8cca39542b0a894f24efb499c75a7906a5199c9b
ismail saoudi <ismail.saoudi@icloud.com>
2022-09-07 21:44:02 -0400

    make it better

--- server.js
+++ server.js
@@ -35,15 +35,6 @@ app.use(express.urlencoded({
     extended: true
 }));
 
-// declare react files in build as static
-app.use(express.static(path.join(__dirname, "build")));
-
-// serve index.html from the build folder
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
-
 
 app.use(routes);
 
